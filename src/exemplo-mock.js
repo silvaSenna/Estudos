bd819ae19957da846b348fd791821575acecd23c
ayrton.meireles <ayrton.meireles@nutrien.com>
2022-08-08 15:19:21 -0300

    erro eslint

--- src/exemplo-mock.js
+++ src/exemplo-mock.js
@@ -1,18 +1,18 @@
-function realizarParaAdultos(pessoas, callback) {
+function realizarParaAdultos (pessoas, callback) {
   for (let i = 0; i < pessoas.length; i += 1) {
     if (pessoas[i].idade >= 18) {
-      callback(pessoas[i]);
+      callback(pessoas[i])
     }
   }
 }
 
-function aguardarTimer(callback) {
+function aguardarTimer (callback) {
   setTimeout(() => {
-    callback();
-  }, 3000);
+    callback()
+  }, 3000)
 }
 
 module.exports = {
   realizarParaAdultos,
-  aguardarTimer,
-};
+  aguardarTimer
+}
